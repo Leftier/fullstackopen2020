488df5a5f26db7297d74f076951624b02d7abc9a
Shai Léger <shaileger@gmail.com>
2020-11-14 20:33:51 -0600

    Part 3 | Exercise 3.3: Phonebook backend step3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -25,11 +25,27 @@ app.get('/', (request, response) => {
   response.json(persons)
 })
 
+app.get('/persons', (request, response) => {
+  response.json(persons)
+})
+
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 }) 
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  console.log(id, person);
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(PORT);
+  console.log(`Server running on port ${PORT}`)
 })
