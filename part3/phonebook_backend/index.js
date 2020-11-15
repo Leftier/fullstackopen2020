322eb4b1c4b5513855fa242a569243bfddb793f4
Shai Léger <shaileger@gmail.com>
2020-11-14 20:48:53 -0600

    Part 3 | Exercise 3.5: Phonebook backend step5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -25,10 +25,6 @@ let persons = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.json(persons)
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
@@ -54,6 +50,18 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 2000)
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
